253f09194c58e41a897adc15cc224ed78d3859fe
masato-ka <nsb21708@nifty.com>
2018-04-01 11:31:30 +0900

    Amend SimManager.js for simplify.

--- out/src/commands/SimManager.js
+++ out/src/commands/SimManager.js
@@ -10,8 +10,178 @@ class SimManager{
         this.client = client;
     }
 
+    simDetailsUI(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Get sim info", "_requestSimDetails");     
+    }
 
-    chooseSimList(sims){
+    changeSimSpeedUI(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Change sim speed", "_requestChangeSpeed");
+    }
+
+    changeSimNickNameUI(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Change sim name",'_requestChangeSimNickName');
+    }
+    
+    addNewTagsUI(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Add new tags",'_requestAddNewTags');
+    }
+
+    deleteTagsUI(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Delete tags", "_requestDeleteTag");
+    }
+
+    getSoracomAirStats(soracomAirSim){
+        this._commandHandler(soracomAirSim, "SIM Stats", "_requestSoracomAirStats");
+    }
+
+    getSoracomBeamStats(soracomAirSim){
+        this._commandHandler(soracomAirSim, "Beam Stats", "_requestSoracomBeamStats");
+    }
+
+    _commandHandler(soracomAirSim, commandHeader, requestFunctionName){
+        this._getImsi(soracomAirSim).then(result=>{
+            return this[requestFunctionName](result);
+        }).then(result=>{
+            this.showResultToOutput("[SORACOM SIM Manager]["+commandHeader+"]:", result);
+        }).catch(error=>{
+            vscode.window.showErrorMessage(error);
+        })
+    }
+
+    _getImsi(soracomAirSim){
+        return new Promise((resolve,reject)=>{
+            if(!soracomAirSim){
+                this.client.getSimList().then(result=>{
+                    return this.formatSimList(result);
+                }).then(result=>{
+                    return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})
+                }).then(result=>{
+                    var imsi = result.split(":")[0];                    
+                    if(imsi===""){
+                        return reject(new Error('Invalid IMSI number'));
+                    }
+                    resolve(imsi)
+                })
+            }else{
+                try{
+                    var imsi = soracomAirSim['imsi'];
+                    if(imsi===""){
+                        return reject(new Error('Invalid IMSI number'));
+                    }
+                    resolve(imsi);
+                }catch(e){
+                    return resolve(new Error('Invalid IMSI nuber'));
+                }
+            }
+        })
+    }
+
+    _requestSimDetails(imsi){
+        return new Promise((resolve,reject)=>{
+            this.client.getSimDetails(imsi)
+            .then(result=>{
+                resolve(result);
+            }).catch(error=>{
+                return reject(error['message']);
+            })
+        })
+    }
+
+    _requestChangeSpeed(imsi){
+        return new Promise((resolve,reject)=>{
+                        vscode.window.showQuickPick(["s1.minimum","s1.slow","s1.standard","s1.fast"],
+                            {placeHolder: "Select new SIM speed"}).then(result=>{
+                                this.client.updateSimSpeed(imsi,result)
+                                .then(result=>{
+                                    resolve(result);
+                                }).catch(error=>{
+                                    return reject(error['message']);
+                                })
+                        })    
+                    });
+       
+    }
+
+    _requestChangeSimNickName(imsi){
+        return new Promise((resolve,reject)=>{
+            vscode.window.showInputBox({placeHolder:"Input SIM name"}).then(result=>{
+                this.client.updateSimName(imsi,result).then(result=>{
+                    resolve(result);
+                }).catch(error=>{
+                    return reject(error['message']);
+                })
+            })
+        })
+    }
+
+    _requestAddNewTags(imsi){
+        return new Promise((resolve,reject)=>{
+            vscode.window.showInputBox({placeHolder:"Input tags example:[{\"tagName\":\"name\",\"tagValue\":\"value\"},...]"})
+            .then(result=>{
+                this.client.addNewTags(imsi,JSON.parse(result)).then(result=>{
+                    resolve(result);
+                }).catch(error=>{
+                    return reject(error['message']);
+                })
+            });
+        })
+    }
+
+    _requestDeleteTag(imsi){
+        return new Promise((resolve,reject)=>{
+            vscode.window.showInputBox({placeHolder:"Input tags key for delete."}).then(result=>{
+                this.client.deleteTag(imsi, result).then(result=>{
+                    resolve(result);
+                }).catch(error=>{
+                    return reject(error['message']);
+                })
+            })
+        });
+    }
+
+    _requestSoracomAirStats(imsi){
+        return new Promise((resolve,reject)=>{
+            vscode.window.showInputBox(
+                {placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
+                    .then(result=>{
+                        try{
+                            var query = JSON.parse(result);
+                            return this.client.getStatsAirSubscriber(imsi,query)
+                                .catch(error=>{
+                                    console.log('error');
+                                    return reject(error['message']);
+                                });
+                        }catch(SyntaxError){
+                            return reject(new Error("Failed parse query: " + result))
+                        }
+                    }).then(result=>{
+                        resolve(result);
+                    })
+        })
+    }
+
+    _requestSoracomBeamStats(imsi){
+        return new Promise((resolve,reject)=>{
+            vscode.window.showInputBox(
+                {placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
+                    .then(result=>{
+                        try{
+                            var query = JSON.parse(result);
+                            return this.client.getStatsBeamSubscriber(imsi,query)
+                                .catch(error=>{
+                                    console.log('error');
+                                    reject(error['message']);
+                                });
+                        }catch(SyntaxError){
+                            return reject(new Error("Failed parse query: " + result))
+                        }
+                    }).then(result=>{
+                        resolve(result);
+                    })
+        })
+    }
+
+    formatSimList(sims){
         return new Promise(resolve=>{
             const chooseList =[];
             sims.forEach(item=>{    
@@ -21,226 +191,11 @@ class SimManager{
         })
     }
 
-    changeSpeed(imsi, list, description){
-        return new Promise(resolve=>{
-                        vscode.window.showQuickPick(list,{placeHolder: description}).then(result=>{
-                            return this.client.updateSimSpeed(imsi,result);
-                        }).then(result=>{resolve(result)})            
-                    });
-       
-    }
-
     showResultToOutput(header, body){
         this.outputChannel.appendLine(header);
         this.outputChannel.append(JSON.stringify(body));
         this.outputChannel.show(true);
     }
-
-    simDetailsUI(soracomAirSim){
-                
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result, {placeHolder:"Select SIM"});
-            }).then(result=>{
-                const imsi = result.split(":")[0];
-                return this.client.getSimDetails(imsi);
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][GET SIM Info]:", result);
-            });
-        }else{
-            this.client.getSimDetails(soracomAirSim['imsi']).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][GET SIM Info]:", result);
-                console.log(result);
-            })
-        }
-    }
-
-    changeSimSpeedUI(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result, {placeHolder:"Select SIM"});
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return this.changeSpeed(imsi,["s1.minimum","s1.slow","s1.standard","s1.fast"], "Select new SIM speed");
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][CHANGE SIM Speed]:", result);
-            })
-        }else{
-            this.changeSpeed(soracomAirSim['imsi']).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][CHANGE SIM Speed]:", result);
-            })
-        }
-    }
-
-    changeSimNickNameUI(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return new Promise(resolve=>{
-                    vscode.window.showInputBox({placeHolder:"Input SIM name"}).then(result=>{
-                        this.client.updateSimName(imsi,result).then(result=>{
-                            resolve(result);
-                        });
-                    })
-                })
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][CHANGE SIM NAME]:",result);
-            });
-        }else{
-            vscode.window.showInputBox({placeHolder:"Input SIM name"}).then(result=>{
-                return this.client.updateSimName(soracomAirSim['imsi'],result);
-            }).then(result=>{                
-                this.showResultToOutput("[SORACOM SIM Manager][CHANGE SIM NAME]:",result);
-            })
-        }
-    }
-
-    addNewTagsUI(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})                
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return new Promise(resolve=>{
-                    vscode.window.showInputBox({placeHolder:"Input tags example:[{\"tagName\":\"name\",\"tagValue\":\"value\"}]"})
-                    .then(result=>{
-                        this.client.addNewTags(imsi,JSON.parse(result)).then(result=>{
-                            resolve(result);
-                        });
-                    });
-                })
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][ADD TAGS]:", result);
-            })
-        }else{
-            var imsi = soracomAirSim['imsi'];
-            vscode.window.showInputBox({placeHolder:"Input tags example:[{\"tagName\":\"name\",\"tagValue\":\"value\"}]"})
-            .then(result=>{
-                return this.client.addNewTags(imsi,JSON.parse(result));
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][ADD TAGS]:", result);
-            })
-        }
-    }
-
-    deleteTagsUI(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result)
-            }).then(result=>{
-                return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})                
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return new Promise(resolve=>{
-                    vscode.window.showInputBox({placeHolder:"Input tags key for delete."}).then(result=>{
-                        this.client.deleteTag(imsi, result).then(result=>{
-                            resolve(result);
-                        })
-                    })
-                });
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][DELETE TAGS]:",result);
-            })
-        }else{
-            var imsi = soracomAirSim['imsi'];
-            vscode.window.showInputBox({placeHolder:"Input tags key for delete."}).then(result=>{
-                return this.client.deleteTag(imsi,result);
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][DELETE TAGS]:",result);
-            })
-        }
-    }
-
-    getSoracomAirStats(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return new Promise(resolve=>{
-                    vscode.window.showInputBox({placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
-                        .then(result=>{
-                            try{
-                                var query = JSON.parse(result);
-                                return this.client.getStatsAirSubscriber(imsi,query);
-                            }catch(SyntaxError){
-                                vscode.window.showErrorMessage("failed parse query");
-                            }
-                        }).then(result=>{
-                            resolve(result);
-                        })
-                })
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][SIM Stats]", result);
-            })
-        }else{
-            var imsi = soracomAirSim['imsi'];
-            vscode.window.showInputBox({placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
-            .then(result=>{
-                try{
-                    var query = JSON.parse(result);
-                    return this.client.getStatsAirSubscriber(imsi,query);
-                }catch(SyntaxError){
-                    vscode.window.showErrorMessage("failed parse query");
-                }
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][SIM Stats]", result);
-            })
-        }
-    }
-
-    getSoracomBeamStats(soracomAirSim){
-        if(!soracomAirSim){
-            this.client.getSimList().then(result=>{
-                return this.chooseSimList(result);
-            }).then(result=>{
-                return vscode.window.showQuickPick(result,{placeHolder:"Select SIM"})
-            }).then(result=>{
-                var imsi = result.split(":")[0];
-                return new Promise(resolve=>{
-                    vscode.window.showInputBox({placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
-                        .then(result=>{
-                            try{
-                                var query = JSON.parse(result);
-                                return this.client.getStatsBeamSubscriber(imsi,query);
-                            }catch(SyntaxError){
-                                vscode.window.showErrorMessage("failed parse query");
-                            }
-                        }).then(result=>{
-                            resolve(result);
-                        })
-                })
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][Beam Stats]", result);
-            })
-        }else{
-            var imsi = soracomAirSim['imsi'];
-            vscode.window.showInputBox({placeHolder:"Input query {from:yyyy-MM-ddTHH:mm:ss,to:yyyy-MM-ddTHH:mm:ss,period:minutes}"})
-            .then(result=>{
-                try{
-                    var query = JSON.parse(result);
-                    return this.client.getStatsBeamSubscriber(imsi,query);
-                }catch(SyntaxError){
-                    vscode.window.showErrorMessage("failed parse query");
-                }
-            }).then(result=>{
-                this.showResultToOutput("[SORACOM SIM Manager][Beam Stats]", result);
-            })
-        }
-    }
-
 
 }
 
