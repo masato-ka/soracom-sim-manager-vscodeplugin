c5f021472c61595131794d2776ac2725dea27494
masato-ka <nsb21708@nifty.com>
2018-04-03 07:33:28 +0900

    Add function group list

--- out/src/commands/GroupManager.js
+++ out/src/commands/GroupManager.js
@@ -9,8 +9,31 @@ http://opensource.org/licenses/mit-license.php
 
 "use strict";
 
+const vscode = require('vscode');
 
 class GroupManager{
+
+   constructor(context,client){
+        this.outputChannel = vscode.window.createOutputChannel("SoracomSimManager");
+        this.context = context;
+        this.client = client;
+
+    }
+
+    getGroupList(){
+        this.client.getGroups().then(result=>{
+            this.showResultToOutput()
+        }).catch(error=>{
+
+        })
+    }
+
+    showResultToOutput(header, body){
+        this.outputChannel.appendLine(header);
+        this.outputChannel.append(JSON.stringify(body));
+        this.outputChannel.show(true);
+    }
+
 
 }
 
